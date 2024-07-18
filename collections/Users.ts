b64c01ab4917cd436a3f41b103610e99c7f940d4
sannidhya <snsannidhya1512@gmail.com>
2024-07-19 01:43:41 +0530

    sending email verification

--- collections/Users.ts
+++ collections/Users.ts
@@ -2,7 +2,18 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        // return PrimaryActionEmailHtml({
+        //   actionLabel: "verify your account",
+        //   buttonText: "Verify Account",
+        //   href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+        // })
+        return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'></a>`;
+      },
+    },
+  },
   access: {
     read: () => true,
     create: () => true,
